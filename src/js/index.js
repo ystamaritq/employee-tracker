12d683a96d26443386656e96857340df5c49030b
Yadira Tamarit <ystamaritq@gmail.com>
2020-07-08 17:10:15 -0500

    added employee questions and control flow to see if Add Employee was selected

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,13 @@
-const inquirer = require("inquirer");
-const { promptListActions } = require("./prompts");
+const { promptListActions, promptAddEmployeeQuestions } = require("./prompts");
 const { handleError, instructionsMessage } = require("./utils");
 
 async function init() {
 	try {
 		instructionsMessage();
 		const actionSelected = await promptListActions();
+		if (actionSelected == "Add Employee") {
+			const employee = await promptAddEmployeeQuestions();
+		}
 	} catch (err) {
 		handleError(err);
 	}
