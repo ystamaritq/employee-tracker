f741db0b0626caff8959e8e28623ba1e0d1457d1
Yadira Tamarit <ystamaritq@gmail.com>
2020-07-08 00:32:48 -0500

    added schema employee

---
+++ src/sql/employeeTracker.sql
@@ -0,0 +1,48 @@
+DROP DATABASE IF EXISTS employeeTracker;
+
+CREATE DATABASE employeeTracker;
+
+USE employeeTracker;
+
+CREATE TABLE department
+(
+    id INT NOT NULL
+    AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR
+    (30) NOT NULL
+);
+
+
+    CREATE TABLE role
+    (
+        id INT NOT NULL
+        AUTO_INCREMENT PRIMARY KEY,
+	title VARCHAR
+        (30) NOT NULL,
+	salary DECIMAL
+        (6) NOT NULL, 
+    department_id INT NOT NULL,
+	FOREIGN KEY
+        (department_id) REFERENCES department
+        (id)
+);
+
+        CREATE TABLE employee
+        (
+            id INT NOT NULL
+            AUTO_INCREMENT PRIMARY KEY,
+	first_name VARCHAR
+            (30) NOT NULL,
+	last_name VARCHAR
+            (30) NOT NULL,
+	role_id INT NOT NULL,
+    FOREIGN KEY
+            (role_id) REFERENCES role
+            (id),
+	manager_id INT NULL,
+    FOREIGN KEY
+            (manager_id) REFERENCES employee
+            (id)
+);
+
+            
