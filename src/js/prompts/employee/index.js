03fd7865d8e6fcdd603c7653a4fdcaf2b7d5f7a0
Yadira Tamarit <ystamaritq@gmail.com>
2020-07-09 14:50:03 -0500

    added employee async functions

--- src/js/prompts/employee/index.js
+++ src/js/prompts/employee/index.js
@@ -40,6 +40,13 @@ const viewEmployeesQuestions = [
 
 const updateEmployeeQuestions = [
 	{
+		type: "list",
+		prefix: "*".cyan.bold,
+		message: "Please select the employee to update",
+		name: "update",
+		choices: ["test1", "test2", "test3"],
+	},
+	{
 		type: "input",
 		prefix: "*".cyan.bold,
 		message: "Please enter the employee first name",
@@ -61,6 +68,7 @@ const removeEmployeeQuestions = [
 		prefix: "*".cyan.bold,
 		message: "Please select the employee to remove",
 		name: "remove",
+		choices: ["test1", "test2", "test3"],
 	},
 ];
 
@@ -70,6 +78,7 @@ const employeesByDepartmentQuestions = [
 		prefix: "*".cyan.bold,
 		message: "Please select a department to display the employees",
 		name: "by_department",
+		choices: ["test1", "test2", "test3"],
 	},
 ];
 
@@ -79,6 +88,7 @@ const employeesByManagerQuestions = [
 		prefix: "*".cyan.bold,
 		message: "Please select the manager to display the employees",
 		name: "by_manager",
+		choices: ["test1", "test2", "test3"],
 	},
 ];
 
@@ -88,18 +98,75 @@ const employeesByRoleQuestions = [
 		prefix: "*".cyan.bold,
 		message: "Please select the role to display the employees",
 		name: "by_role",
+		choices: ["test1", "test2", "test3"],
 	},
 ];
+
 // ENDS - Employee questions
 
-// START - functions
+// START - Functions
 
+// add employees function
 async function addEmployee() {
-	console.log(` \nEnter Employee's Info \n`.cyan.bold.dim.italic);
+	console.log(` \n Enter Employee's Info \n`.cyan.bold.dim.italic);
 	const info = await inquirer.prompt(addEmployeeQuestions);
 	return new Employee(null, info.first_name, info.last_name, info.role, null);
 }
 
-async function viewEmployees
+// view employees function
+async function viewEmployees() {
+	console.log(` \n Employees List \n`.cyan.bold.dim.italic);
+	const info = await inquirer.prompt(viewEmployeesQuestions);
+	return info.view;
+}
 
-module.exports = { addEmployee };
+// update employees function
+async function updateEmployee() {
+	console.log(` \n Update Employee's Info \n`.cyan.bold.dim.italic);
+	const info = await inquirer.prompt(updateEmployeeQuestions);
+	return new Employee(
+		null,
+		info.update_first_name,
+		info.update_last_name,
+		null,
+		null
+	);
+}
+
+// remove employee function
+async function removeEmplosyee() {
+	console.log(` \n Remove Employee \n`.cyan.bold.dim.italic);
+	const info = await inquirer.prompt(removeEmployeeQuestions);
+	return console.log("Employee removed");
+}
+
+// view employees by department function
+async function employeesByDepartment() {
+	console.log(` \n Employees by Department \n`.cyan.bold.dim.italic);
+	const info = await inquirer.prompt(employeesByDepartmentQuestions);
+	return info.by_department;
+}
+
+//  view employees by manager function
+async function employeesByManager() {
+	console.log(` \n Employees by Manager \n`.cyan.bold.dim.italic);
+	const info = await inquirer.prompt(employeesByManagerQuestions);
+	return info.by_manager;
+}
+
+// view employees by role
+async function employeesByRole() {
+	console.log(` \n Employees by Role \n`.cyan.bold.dim.italic);
+	const info = await inquirer.prompt(employeesByRoleQuestions);
+	return info.by_role;
+}
+
+module.exports = {
+	addEmployee,
+	viewEmployees,
+	updateEmployee,
+	removeEmplosyee,
+	employeesByDepartment,
+	employeesByManager,
+	employeesByRole,
+};
