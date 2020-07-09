f21e7adf1280e5735d756bcf68d842c4bbd7cd9e
Yadira Tamarit <ystamaritq@gmail.com>
2020-07-08 23:48:13 -0500

    add role class

--- src/js/models/Role.js
+++ src/js/models/Role.js
@@ -1,17 +1,11 @@
 // TODO: Write code to define and export the Employee class
 
 class Role {
-	constructor(tittle, salary) {
+	constructor(id, tittle, salary, department_id) {
+		this.id = id;
 		this.tittle = tittle;
 		this.salary = salary;
-	}
-
-	getTitle() {
-		return this.tittle;
-	}
-
-	getSalary() {
-		return this.salary;
+		this.department_id = department_id;
 	}
 }
 
