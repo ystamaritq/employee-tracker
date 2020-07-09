a08707e5733067bd15b3ec74d676964c99ff3555
Yadira Tamarit <ystamaritq@gmail.com>
2020-07-08 23:48:02 -0500

    added employee class model

--- src/js/models/Employee.js
+++ src/js/models/Employee.js
@@ -1,17 +1,12 @@
 // TODO: Write code to define and export the Employee class
 
 class Employee {
-	constructor(first_name, last_name) {
+	constructor(id, first_name, last_name, role_id, manager_id) {
+		this.id = id;
 		this.first_name = first_name;
 		this.last_name = last_name;
-	}
-
-	getFirstName() {
-		return this.first_name;
-	}
-
-	getLastName() {
-		return this.last_name;
+		this.role_id = role_id;
+		this.manager_id = manager_id;
 	}
 }
 
