cbae95cc6931c1abcfe9225ffc3219c5c52529c7
Yadira Tamarit <ystamaritq@gmail.com>
2020-07-09 16:01:32 -0500

    added const functions to the main prompts js  file

--- src/js/prompts/index.js
+++ src/js/prompts/index.js
@@ -3,7 +3,17 @@ const { cyan } = require("colors");
 const Employee = require("../models/Employee");
 const Role = require("../models/Role");
 const Department = require("../models/Department");
-const { addEmployee } = require("./employee");
+const {
+	addEmployee,
+	viewEmployees,
+	updateEmployee,
+	removeEmployee,
+	employeesByDepartment,
+	employeesByManager,
+	employeesByRole,
+	updateEmployeeRole,
+	updateEmployeeManager,
+} = require("./employee");
 const department_prompts = require("./department");
 const roles_prompts = require("./role");
 
@@ -37,55 +47,53 @@ const actions = [
 
 async function showActions() {
 	const answers = await inquirer.prompt(actions);
-	while (answers) {
-		switch (answers.action) {
-			case "Add Employee":
-				addEmployee();
-				break;
-			case "View All Employees":
-				viewEmployees();
-				break;
-			case "Update Employee":
-				updateEmployee();
-				break;
-			case "Remove Employee":
-				removeEmployee();
-				break;
-			case "Update Employee Role":
-				updateEmployeeRole();
-				break;
-			case "Update Employee Manager":
-				updateEmployeeManager();
-				break;
-			case "View All Employees by Department":
-				break;
-			case "View All Employees by Manager":
-				employeesByDepartment();
-				break;
-			case "View All Employees by Role":
-				employeesByRole();
-				break;
-			case "Add Role":
-				break;
-			case "View All Roles":
-				break;
-			case "Update Role":
-				break;
-			case "Remove Role":
-				break;
-			case "Add Department":
-				break;
-			case "View All Departments":
-				break;
-			case "Update Department":
-				break;
-			case "Remove Department":
-				break;
-		}
+	switch (answers.action) {
+		case "Add Employee":
+			addEmployee();
+			break;
+		case "View All Employees":
+			viewEmployees();
+			break;
+		case "Update Employee":
+			updateEmployee();
+			break;
+		case "Remove Employee":
+			removeEmployee();
+			break;
+		case "Update Employee Role":
+			updateEmployeeRole();
+			break;
+		case "Update Employee Manager":
+			updateEmployeeManager();
+			break;
+		case "View All Employees by Department":
+			employeesByDepartment();
+			break;
+		case "View All Employees by Manager":
+			employeesByManager();
+			break;
+		case "View All Employees by Role":
+			employeesByRole();
+			break;
+		case "Add Role":
+			break;
+		case "View All Roles":
+			break;
+		case "Update Role":
+			break;
+		case "Remove Role":
+			break;
+		case "Add Department":
+			break;
+		case "View All Departments":
+			break;
+		case "Update Department":
+			break;
+		case "Remove Department":
+			break;
 	}
-	return console.log("Finish");
 }
 
 module.exports = {
-	showActions: showActions,
+	showActions,
 };
